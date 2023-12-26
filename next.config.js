3d89fb85b532109ae594841fb07c280cbf1fa0b0
borjasff <borjasff3@gmail.com>
2023-12-26 19:13:05 +0100

    deploy of env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    backendURL: 'http://127.0.0.1:4000',
-    frontendURL: 'http://localhost:3000'
+    backendURL: 'https://backend-nodesend.vercel.app/',
+    frontendURL: 'https://frontend-nodesend.vercel.app/'
   }
 }
