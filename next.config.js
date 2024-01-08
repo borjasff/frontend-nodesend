92b75bb99143759ea6f4eb8faa5b820e0e5782c7
borjasff <borjasff3@gmail.com>
2024-01-08 16:44:47 +0100

    deploy of env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    backendURL: 'https://backend-nodesend.vercel.app/',
-    frontendURL: 'https://frontend-nodesend-iyso3qfqo-borjasffs-projects.vercel.app/'
+    backendURL: 'https://backend-nodesend.vercel.app',
+    frontendURL: 'https://frontend-nodesend-iyso3qfqo-borjasffs-projects.vercel.app'
   }
 }
